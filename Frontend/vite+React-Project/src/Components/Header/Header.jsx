9215286eadb2466ef74d38121e3536a895c18821
DrakoZe <maxizenon625@gmail.com>
2024-03-01 19:53:26 -0300

    header

--- Frontend/vite+React-Project/src/Components/Header/Header.jsx
+++ Frontend/vite+React-Project/src/Components/Header/Header.jsx
@@ -4,35 +4,6 @@ function Header() {
 
   return (
     <header>
-<<<<<<< HEAD
-      <nav className="navbar bg-body-tertiary border border-3">
-        <div className="container-fluid">
-          <a className="d-flex align-items-center navbar-brand text-truncate" href="#">
-            <img src="/images/logo.png" alt="Logo" style={{ width: "5rem" }} class="d-inline-block align-text-top" />
-            Recruiting DH
-          </a>
-          <ul class="navbar-nav mb-2 mb-lg-0 d-flex flex-row navbar-nav-scroll">
-            <li class="nav-item me-5">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item me-5">
-              <a class="nav-link" href="#">Aspirants</a>
-            </li>
-            <li class="nav-item me-5">
-              <a class="nav-link" href="#">Professions</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Postularse</a>
-            </li>
-          </ul>
-          <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-              <label class="form-check-label" for="flexSwitchCheckDefault">Modo oscuro</label>
-          </div>
-          <SearchBar />
-        </div>
-      </nav>
-=======
         <div className="container-fluid pb-3 bg-gray-200">
         <nav className="navbar place-content-around">
           
@@ -57,7 +28,6 @@ function Header() {
         <SearchBar/>
 
       </div>
->>>>>>> 627404acdefffdeec8d58fd00f06637060b9c3e1
     </header>
   )
 }
