627404acdefffdeec8d58fd00f06637060b9c3e1
Taguminn <aledanielduran@gmail.com>
2024-02-29 21:35:21 -0300

    Header's style

--- Frontend/vite+React-Project/src/Components/Header/Header.jsx
+++ Frontend/vite+React-Project/src/Components/Header/Header.jsx
@@ -4,29 +4,30 @@ function Header() {
 
   return (
     <header>
-      <nav className="navbar bg-body-tertiary mb-5">
-        <div className="container-fluid">
-          <a className="d-flex align-items-center navbar-brand" href="#">
-            <img src="/images/logo.png" alt="Logo" style={{ width: "100px" }} class="d-inline-block align-text-top" />
-            Recruiting DH
+        <div className="container-fluid pb-3 bg-gray-200">
+        <nav className="navbar place-content-around">
+          
+          <a className="w-8 sm:w-14" href="#">
+            <img src="/images/recruiting-logo.png" alt="Logo" class="img-fluid"/>
           </a>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row">
-              <li class="nav-item me-5">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item me-5">
-                <a class="nav-link" href="#">Aspirants</a>
-              </li>
-              <li class="nav-item me-5">
-                <a class="nav-link" href="#">Professions</a>
-              </li>
-              <li class="nav-item me-5">
-                <a class="nav-link" href="#">Postularse</a>
-              </li>
-            </ul>
-            <SearchBar />
-          </div>
-      </nav>
+
+          <i className="fa-solid fa-house active d-sm-none"></i>
+          <a className="d-sm-block d-none" aria-current="page" href="#">Home</a>
+
+          <i className="fa-solid fa-user active d-sm-none"></i>
+          <a className="d-sm-block d-none" href="#">Aspirants</a>
+
+          <i className="fa-solid fa-briefcase active d-sm-none"></i>
+          <a className="d-sm-block d-none" href="#">Professions</a>
+
+          <i className="fa-solid fa-right-to-bracket active d-sm-none"></i>
+          <a className="d-sm-block d-none" href="#">apply</a>
+
+        </nav>
+
+        <SearchBar/>
+
+      </div>
     </header>
   )
 }
--- Frontend/vite+React-Project/src/Components/Header/SearchBar/SearchBar.jsx
+++ Frontend/vite+React-Project/src/Components/Header/SearchBar/SearchBar.jsx
@@ -1,13 +1,9 @@
-
-
 function SearchBar() {
-
     return (
-        <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <form className="flex">
+            <input className="form-control me-2 bg-slate-100" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
     )
 }
-
 export default SearchBar
