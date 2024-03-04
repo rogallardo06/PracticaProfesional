be3b3ff3f073b9046430b6554a06f590fff1561b
Taguminn <aledanielduran@gmail.com>
2024-03-04 00:14:56 -0300

    Aspirant Style

--- Frontend/vite+React-Project/src/Components/Content/Card/Card.jsx
+++ Frontend/vite+React-Project/src/Components/Content/Card/Card.jsx
@@ -1,12 +1,20 @@
-function Card({imagen, nombre, apellido}) {
+function Card({imagen, nombre, apellido, profession, position, company, yearsOnDuty, email, tel, linkedin}) {
+
 
     return (
-    <div class="card mx-auto mb-5 w-60 dark:bg-stone-700">
-        <img src={imagen} class="card-img-top" alt="imagen de aspirante"/>
-            <div class="card-body dark:bg-zinc-950 dark:text-white">
-                <h5 class="card-title">{nombre} {apellido}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Conocer mas detalles</a>
+    <div className="card mx-auto mb-5 w-72 dark:bg-stone-700">
+        <img src={imagen} className="card-img-top" alt="imagen de aspirante"/>
+            <div className="card-body dark:bg-zinc-950 dark:text-white">
+                <h5 className="card-title font-bold">{nombre} {apellido}</h5>
+                <p className="text-neutral-400">{position} - {profession}</p>
+                <p>{yearsOnDuty} years in {company}</p>
+                {linkedin && (
+                    <div className="flex justify-around mt-8">
+                        <a href={"https://api.whatsapp.com/send?phone=" + "54".concat(tel.replace(/\+|\s/g, ""))}><i class="fa-brands fa-whatsapp"></i></a>
+                        <a href={"mailto:" + email}><i class="fa-regular fa-envelope"></i></a>
+                        <a href={linkedin}><i class="fa-brands fa-linkedin"></i></a>
+                    </div>
+                )}
             </div>
     </div>
     )
